750b88cfef30922efe927b1c44c452bd5ab708c4
Unknown <abc303616@gmail.com>
2018-01-08 18:44:49 +0800

    This reverts commit 02af0d1b3714a92c548f0794bae5cc56cd88a55b.

---
+++ hey.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int a = 0;
+
+void abc()
+{
+    printf("OK");
+}
--- main.cpp
+++ main.cpp
@@ -5,5 +5,6 @@ using namespace std;
 int main()
 {
     cout << "branch3333 world!" << endl;
+    cout << "teastats world!" << endl;
     return 0;
 }
