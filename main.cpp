771b15448a7e0b6bbe4ab9e75025bf5d77e491c8
Unknown <abc303616@gmail.com>
2018-01-08 19:07:52 +0800

    123hey OK

--- main.cpp
+++ main.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 
 using namespace std;
+void abc();
 
 int main()
 {
     cout << "branch3333 world!" << endl;
     cout << "teastats world!" << endl;
+    abc();
     return 0;
 }
